3d0518232299b0e9c9f95a88e28a98c35bbaf0f8
Ye Li <ye.li@nxp.com>
2025-08-04 16:41:50 +0800

    feat(imx94): remove release XSPI GMID
    Because SPL will release the XSPI GMID, TF-A does not need to release
    it again. Although this does not cause an issue, it returns a failure
    response when running the CRRM flash.bin, so remove the redundant
    release GMID call.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>
    Change-Id: I22768d80275fdb5b1c4b180bd02bd57b832a18e0

--- plat/imx/imx9/common/imx9_bl31_setup.c
+++ plat/imx/imx9/common/imx9_bl31_setup.c
@@ -132,11 +132,6 @@ void bl31_platform_setup(void)
 	/* get soc info */
 	ele_get_soc_info();
 
-#if HAS_XSPI_SUPPORT
-	/* i.MX94 specific */
-	ele_release_gmid();
-#endif
-
 	plat_imx9_scmi_setup();
 }
 
