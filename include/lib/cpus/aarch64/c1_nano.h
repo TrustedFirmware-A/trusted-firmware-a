59644572c4ad0ea25c07d1c54b036e9554bb5d93
Min Yao Ng <minyao.ng@arm.com>
2025-09-04 14:16:07 +0200

    chore(tc): align core names to Arm Lumex
    Adopt core names aligned to Arm Lumex [1]
    
    Nevis => C1-Nano
    Gelas => C1-Pro
    Travis => C1-Ultra
    Alto => C1-Premium
    
    C1-Pro TRM: https://developer.arm.com/documentation/107771/0102/
    C1-Ultra TRM: https://developer.arm.com/documentation/108014/0100/
    C1-Premium TRM: https://developer.arm.com/documentation/109416/0100/
    C1-Nano TRM: https://developer.arm.com/documentation/107753/0001/
    
    [1]:
    https://www.arm.com/product-filter?families=c1%20cpus
    https://www.arm.com/products/mobile/compute-subsystems/lumex
    
    Signed-off-by: Min Yao Ng <minyao.ng@arm.com>
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: Id4b487ef6a6fd1b00b75b09c5d06d81bce50a15d
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>

---
+++ include/lib/cpus/aarch64/c1_nano.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023-2025, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef C1_NANO_H
+#define C1_NANO_H
+
+#define C1_NANO_MIDR					U(0x410FD8A0)
+
+/*******************************************************************************
+ * CPU Extended Control register specific definitions
+ ******************************************************************************/
+#define C1_NANO_CPUECTLR_EL1				S3_0_C15_C1_4
+
+/*******************************************************************************
+ * CPU Power Control register specific definitions
+ ******************************************************************************/
+#define C1_NANO_IMP_CPUPWRCTLR_EL1			S3_0_C15_C2_7
+#define C1_NANO_IMP_CPUPWRCTLR_EL1_CORE_PWRDN_EN_BIT	U(1)
+
+#endif /* C1_NANO_H */
--- include/lib/cpus/aarch64/c1_ultra.h
+++ include/lib/cpus/aarch64/c1_ultra.h
@@ -4,20 +4,26 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef NEVIS_H
-#define NEVIS_H
+#ifndef C1_ULTRA_H
+#define C1_ULTRA_H
 
-#define NEVIS_MIDR					U(0x410FD8A0)
+#define C1_ULTRA_MIDR					U(0x410FD8C0)
 
 /*******************************************************************************
  * CPU Extended Control register specific definitions
  ******************************************************************************/
-#define NEVIS_CPUECTLR_EL1				S3_0_C15_C1_4
+#define C1_ULTRA_IMP_CPUECTLR_EL1				S3_0_C15_C1_4
 
 /*******************************************************************************
  * CPU Power Control register specific definitions
  ******************************************************************************/
-#define NEVIS_IMP_CPUPWRCTLR_EL1			S3_0_C15_C2_7
-#define NEVIS_IMP_CPUPWRCTLR_EL1_CORE_PWRDN_EN_BIT	U(1)
+#define C1_ULTRA_IMP_CPUPWRCTLR_EL1			S3_0_C15_C2_7
+#define C1_ULTRA_IMP_CPUPWRCTLR_EL1_CORE_PWRDN_EN_BIT	U(1)
 
-#endif /* NEVIS_H */
+/*******************************************************************************
+ * SME Control registers
+ ******************************************************************************/
+#define C1_ULTRA_SVCRSM					S0_3_C4_C2_3
+#define C1_ULTRA_SVCRZA					S0_3_C4_C4_3
+
+#endif /* C1_ULTRA_H */
