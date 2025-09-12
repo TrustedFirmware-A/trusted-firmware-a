1248db70b9622c4f886d2a443e52f1e7a243022e
Heiko Stuebner <heiko@sntech.de>
2025-09-12 15:32:58 +0200

    feat(rk3568): protect TF-As memory area with the DDR firewall
    Similar to most (all?) other Rockchip SoCs, the RK3568 also has a DDR
    firewall to restrict memory regions to secure usage.
    
    The original submission didn't protect the TF-A's memory region, so
    implement this now with knowledge gained from comments in the OPTEE
    pull-request for the rk356x [0].
    
    Contrary to other SoCs like RK3576 and RK3588, the RK3568 memory
    regions are managed in blocks of 128KB, not blocks of 1MB size.
    With the registers having 16bit for upper and lower block id, this then
    can completely fill the 8GB max ram size the RK356x supports.
    
    The whole behaviour was tested with a number of reads from main u-boot.
    With the current config of protecting everything in the first MB of memory,
    doing any reads before the 1MB border will trigger the expected
    "Synchronous Abort" and restart.
    
    So doing
    => md 0x100000 4
    will succeed, but reading anything before this will trigger the Abort.
    
    [0] https://github.com/OP-TEE/optee_os/pull/6954#issuecomment-3041255974
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Tested-by: Diederik de Haas <diederik@cknow-tech.com>
    Change-Id: I6c75ab2813566e727e46cea86dd229625a64dd3f

--- plat/rockchip/rk3568/drivers/secure/secure.c
+++ plat/rockchip/rk3568/drivers/secure/secure.c
@@ -12,6 +12,34 @@
 #include "secure.h"
 #include <soc.h>
 
+/* unit: Mb */
+static void ddr_fw_rgn_config(uint64_t base_mb, uint64_t top_mb, int rgn_id)
+{
+	if (rgn_id >= FIREWALL_DDR_RGN_CNT || rgn_id < 0) {
+		ERROR("%s regions-id:%d is invalid!\n", __func__, rgn_id);
+		panic();
+	}
+
+	/*
+	 * The firewall regions are managed in 128KB blocks instead of
+	 * the 1MB blocks on the RK3576 and RK3588 SoCs.
+	 */
+	mmio_write_32(FIREWALL_DDR_BASE + FIREWALL_DDR_RGN(rgn_id),
+		      RG_MAP_SECURE(top_mb * 8, base_mb * 8));
+
+	/* enable region */
+	mmio_setbits_32(FIREWALL_DDR_BASE + FIREWALL_DDR_CON, BIT(rgn_id));
+}
+
+static void secure_region_init(void)
+{
+	/* disable all region first except region0 */
+	mmio_clrbits_32(FIREWALL_DDR_BASE + FIREWALL_DDR_CON, 0xfffe);
+
+	/* Use FW_DDR_RGN0_REG to config 0~1M space to secure */
+	ddr_fw_rgn_config(0, 1, 0);
+}
+
 void secure_timer_init(void)
 {
 	mmio_write_32(STIMER0_CHN_BASE(1) + TIMER_CONTROL_REG, TIMER_DIS);
@@ -24,6 +52,8 @@ void secure_timer_init(void)
 
 void sgrf_init(void)
 {
+	secure_region_init();
+
 	/* config all slave as ns for now */
 	mmio_write_32(SGRF_BASE + SGRF_FIREWALL_SLV_CON(0), 0xffff0000);
 	mmio_write_32(SGRF_BASE + SGRF_FIREWALL_SLV_CON(1), 0xffff0000);
@@ -34,6 +64,4 @@ void sgrf_init(void)
 	mmio_write_32(SGRF_BASE + SGRF_FIREWALL_SLV_CON(6), 0xffff0000);
 	mmio_write_32(SGRF_BASE + SGRF_FIREWALL_SLV_CON(7), 0xffff0000);
 	mmio_write_32(SGRF_BASE + SGRF_FIREWALL_SLV_CON(8), 0xffff0000);
-
-	mmio_write_32(FIREWALL_DDR_BASE + FIREWALL_DDR_CON, 0xffff0000);
 }
--- plat/rockchip/rk3568/drivers/secure/secure.h
+++ plat/rockchip/rk3568/drivers/secure/secure.h
@@ -9,7 +9,13 @@
 
 #define SGRF_FIREWALL_SLV_CON(i)	(0x240 + i * 4)
 
+/* ddr firewall */
+#define FIREWALL_DDR_RGN(i)		((i) * 0x4)
+#define FIREWALL_DDR_RGN_CNT		16
 #define FIREWALL_DDR_CON		0x80
+
+#define RG_MAP_SECURE(top, base)	\
+	(((((top) - 1) & 0x7fff) << 16) | ((base) & 0x7fff))
 
 void secure_timer_init(void);
 void sgrf_init(void);
