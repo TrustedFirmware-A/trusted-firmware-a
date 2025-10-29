bec6bd012e9e14e735c13d7fdda0505979e32461
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2025-10-29 17:32:54 -0500

    feat(fvp): enable discovery of two Secure Partitions for live activation
    This patch helps the LFA host to discover the presence of two Secure
    Partitions for FVP platform that are capable of being live activated.
    Necessary attributes of these two Secure Partitions are defined to
    allow the LFA agent to report them to the host when the host performs
    initial discovery of LFA support.
    
    BREAKING CHANGE: Temporarily failing to build
    
    Change-Id: Ibf0405559543cf19aaf45637c11ba5cf2e3a1619
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>

--- include/plat/arm/common/plat_arm_lfa_components.h
+++ include/plat/arm/common/plat_arm_lfa_components.h
@@ -1,11 +1,29 @@
 /*
- * Copyright (c) 2025, Arm Limited. All rights reserved.
+ * Copyright (c) 2025-2026, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #ifndef PLAT_ARM_LFA_COMPONENTS_H
 #define PLAT_ARM_LFA_COMPONENTS_H
+
+/*
+ * Staging area for hosting Secure Partition packages intended for live
+ * activation.
+ */
+#define FVP_SP1_STAGING_MEM_BASE	0x7000000U
+#define FVP_SP2_STAGING_MEM_BASE	0x7100000U
+
+#define FFA_ID_SP1		0x8001
+#define FFA_ID_SP2		0x8002
+
+#define IMAGE_UUID_SECURE_PARTITION_1					\
+	{ { 0xf0, 0x7b, 0x2a, 0x96 }, { 0x1d, 0x47 }, { 0x4d, 0x17 },	\
+	0x9e, 0xc8, { 0x86, 0xa6, 0x4e, 0x25, 0x3e, 0x5c } }
+
+#define IMAGE_UUID_SECURE_PARTITION_2					\
+	{ { 0xc3, 0xff, 0x21, 0x27 }, { 0x7e, 0x41 }, { 0xa9, 0xf8 },	\
+	0x05, 0xaf, { 0x24, 0xa1, 0xaf, 0xa3, 0x34, 0x74 } }
 
 /*
  * Define platform-specific numeric IDs for LFA FVP components.
@@ -19,6 +37,10 @@ typedef enum {
 #if ENABLE_RME
 	LFA_RMM_COMPONENT,
 #endif /* ENABLE_RME */
+#if SUPPORT_SP_LIVE_ACTIVATION
+	LFA_SP1,
+	LFA_SP2,
+#endif
 	LFA_MAX_DEFINED_COMPONENTS
 } fvp_lfa_component_id_t;
 
