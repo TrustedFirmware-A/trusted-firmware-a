4cf39ffc7d6d109cdd834f25684b083fcf4b020d
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2025-10-30 18:00:49 +0100

    feat(qti): extend interrupt dispatcher
    Expand the interrupt dispatcher so that device interrupts can be routed
    to the new in-tree drivers instead of relying on QTISECLIB.
    
    Change-Id: Ic23702ce27eaf18d6d01b151aabc18fb8b02196d
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

--- plat/qti/common/inc/qti_interrupt_svc.h
+++ plat/qti/common/inc/qti_interrupt_svc.h
@@ -7,6 +7,10 @@
 #ifndef QTI_INTERRUPT_SVC_H
 #define QTI_INTERRUPT_SVC_H
 
+typedef  void* (*qti_int_svc_isr_t)(uint32_t id, void *ctx);
+
+int qti_interrupt_svc_register(uint32_t id, qti_int_svc_isr_t func, void *ctx);
+int qti_interrupt_svc_unregister(uint32_t id);
 int qti_interrupt_svc_init(bool have_sel1);
 
 #endif /* QTI_INTERRUPT_SVC_H */
