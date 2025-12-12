d61856ce5698b2c51bcd76e150cd3e59e1584794
Jaiprakash Singh <jaiprakashs@marvell.com>
2025-12-11 23:17:24 -0800

    fix(cpus): enable Neoverse-V2 external LLC support
    Change-Id: I9582c7405db6862e77db240822e241d4082966f2
    Signed-off-by: Jaiprakash Singh <jaiprakashs@marvell.com>
    (cherry picked from commit 1df0bb505644bb00d2e700e4baeea2c1ab730de4)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -54,6 +54,7 @@ CPU_FLAG_LIST += WORKAROUND_CVE_2025_0647
 # Flags to indicate internal or external Last level cache
 # By default internal
 CPU_FLAG_LIST += NEOVERSE_Nx_EXTERNAL_LLC
+CPU_FLAG_LIST += NEOVERSE_Vx_EXTERNAL_LLC
 
 # Flag to enable or disable hardware prefetcher for Neoverse N2 CPU
 # By default enabled
