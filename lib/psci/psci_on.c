03d0ef1ef1f56ce22dfb6f1b47a713b33b5c54b3
Suraj Kakade <suraj.hanumantkakade@amd.com>
2026-01-12 16:41:16 +0530

    fix(psci): do not pass unused parameters
    MISRA violation C2012-2.7:
    A function should not contain unused parameters.
    Removed the unused parameter to fix this violation.
    
    Change-Id: Ib62965bd313389fe9853d7ecdf850c5e7deef766
    Signed-off-by: Suraj Kakade <suraj.hanumantkakade@amd.com>

--- lib/psci/psci_on.c
+++ lib/psci/psci_on.c
@@ -59,8 +59,7 @@ static int cpu_on_validate_state(aff_info_state_t aff_state)
  * The state of all the relevant power domains are changed after calling the
  * platform handler as it can return error.
  ******************************************************************************/
-int psci_cpu_on_start(u_register_t target_cpu,
-		      const entry_point_info_t *ep)
+int psci_cpu_on_start(u_register_t target_cpu)
 {
 	int rc;
 	aff_info_state_t target_aff_state;
