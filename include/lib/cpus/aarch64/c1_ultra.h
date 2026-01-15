76680356eaa5d3e8bb23f709d0a03dfb2f26a947
Yann Gautier <yann.gautier@st.com>
2026-01-15 17:48:46 +0100

    chore(tc): align core names to Arm Lumex
    Adopt core names aligned to Arm Lumex [1]
    
    Nevis => C1-Nano
    Gelas => C1-Pro
    Travis => C1-Ultra
    
    C1-Pro TRM: https://developer.arm.com/documentation/107771/0102/
    C1-Ultra TRM: https://developer.arm.com/documentation/108014/0100/
    C1-Nano TRM: https://developer.arm.com/documentation/107753/0001/
    
    [1]:
    https://www.arm.com/product-filter?families=c1%20cpus
    https://www.arm.com/products/mobile/compute-subsystems/lumex
    
    This is a partial cherry-pick, as Alto (C1-Premium) and TC4 are not
    supported on LTS-v2.10 branch.
    
    Signed-off-by: Min Yao Ng <minyao.ng@arm.com>
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: Id4b487ef6a6fd1b00b75b09c5d06d81bce50a15d
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>
    (cherry picked from commit 7dae0451dda5074191c3ecfdec5eece768c28212)

--- include/lib/cpus/aarch64/c1_ultra.h
+++ include/lib/cpus/aarch64/c1_ultra.h
@@ -1,25 +1,23 @@
 /*
- * Copyright (c) 2023-2024, Arm Limited. All rights reserved.
+ * Copyright (c) 2023-2025, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef CORTEX_GELAS_H
-#define CORTEX_GELAS_H
+#ifndef C1_ULTRA_H
+#define C1_ULTRA_H
 
-#include <lib/utils_def.h>
-
-#define CORTEX_GELAS_MIDR				U(0x410FD8B0)
+#define C1_ULTRA_MIDR					        U(0x410FD8C0)
 
 /*******************************************************************************
  * CPU Extended Control register specific definitions
  ******************************************************************************/
-#define CORTEX_GELAS_IMP_CPUECTLR_EL1			S3_0_C15_C1_5
+#define C1_ULTRA_IMP_CPUECTLR_EL1				S3_0_C15_C1_4
 
 /*******************************************************************************
  * CPU Power Control register specific definitions
  ******************************************************************************/
-#define CORTEX_GELAS_CPUPWRCTLR_EL1			S3_0_C15_C2_7
-#define CORTEX_GELAS_CPUPWRCTLR_EL1_CORE_PWRDN_BIT	U(1)
+#define C1_ULTRA_IMP_CPUPWRCTLR_EL1				S3_0_C15_C2_7
+#define C1_ULTRA_IMP_CPUPWRCTLR_EL1_CORE_PWRDN_EN_BIT		U(1)
 
-#endif /* CORTEX_GELAS_H */
+#endif /* C1_ULTRA_H */
