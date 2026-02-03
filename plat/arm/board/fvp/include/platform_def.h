ARM-software/arm-trusted-firmware

commit 8a583b9783556e656c53c9e85da496a88b42fdf3

refactor(fvp): use crypto-agile measured boot
Update the FVP measured boot flow to use the crypto-agile API. Replace
the previous single-algorithm hash configuration with dynamic algorithm
selection. Align image measurement and event log header generation with
the new hashing model and update platform glue code accordingly.

Change-Id: I4128a0c66a56df6c473c47a577d86cd38bf057f6
Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

plat/arm/board/fvp/include/platform_def.h
@@ -553,7 +553,7 @@ FVP_TRUSTED_SRAM_SIZE == 512
  * Account for additional measurements of secure partitions and SPM.
  * Also, account for OP-TEE running with maximum number of SPs.
  */
-#define PLAT_ARM_EVENT_LOG_MAX_SIZE		UL(0x800)
+#define PLAT_ARM_EVENT_LOG_MAX_SIZE		UL(0x1000)
 #elif defined(IMAGE_BL1) && TRANSFER_LIST
 #define PLAT_ARM_EVENT_LOG_MAX_SIZE		UL(0x200)
 #else

plat/arm/board/fvp/fvp_bl1_measured_boot.c
@@ -24,12 +24,6 @@ static uint8_t *event_log;
 static uint8_t event_log[PLAT_ARM_EVENT_LOG_MAX_SIZE];
 #endif
 
-static const struct event_log_hash_info crypto_hash_info = {
-	.func = crypto_mod_calc_hash,
-	.ids = (const uint32_t[]){ CRYPTO_MD_ID },
-	.count = 1U,
-};
-
 /* FVP table with platform specific image IDs, names and PCRs */
 const event_log_metadata_t fvp_event_log_metadata[] = {
 	{ FW_CONFIG_ID, MBOOT_FW_CONFIG_STRING, PCR_0 },
@@ -43,6 +37,20 @@ void bl1_plat_mboot_init(void)
 {
 	size_t event_log_max_size;
 	int rc;
+	tpm_alg_id algos[] = {
+#ifdef TPM_ALG_ID
+		TPM_ALG_ID,
+#else
+		/*
+		 * TODO: with MEASURED_BOOT=1 several algorithms are now compiled into
+		 * Mbed-TLS, we ought to query the backend to figure out what algorithms
+		 * to use.
+		 */
+		TPM_ALG_SHA256,
+		TPM_ALG_SHA384,
+		TPM_ALG_SHA512,
+#endif
+	};
 
 #if TRANSFER_LIST
 	event_log_max_size = PLAT_ARM_EVENT_LOG_MAX_SIZE;
@@ -55,13 +63,13 @@ void bl1_plat_mboot_init(void)
 #endif
 
 	rc = event_log_init_and_reg(event_log, event_log + event_log_max_size,
-				    &crypto_hash_info);
+				    0U, crypto_mod_tcg_hash);
 	if (rc < 0) {
 		ERROR("Failed to initialize event log (%d).\n", rc);
 		panic();
 	}
 
-	rc = event_log_write_header();
+	rc = event_log_write_header(algos, ARRAY_SIZE(algos), 0, NULL, 0);
 	if (rc < 0) {
 		ERROR("Failed to write event log header (%d).\n", rc);
 		panic();

plat/arm/board/fvp/fvp_bl2_measured_boot.c
@@ -10,6 +10,7 @@
 #if TRANSFER_LIST
 #include <tpm_event_log.h>
 #endif
+#include <common/measured_boot.h>
 #include <drivers/auth/crypto_mod.h>
 #include <drivers/measured_boot/metadata.h>
 #include <event_measure.h>
@@ -30,13 +31,7 @@ CASSERT(ARM_EVENT_LOG_DRAM1_SIZE >= PLAT_ARM_EVENT_LOG_MAX_SIZE, \
 #endif /* defined(SPD_tspd) || defined(SPD_opteed) || defined(SPD_spmd) */
 
 /* Event Log data */
-static uint64_t event_log_base;
-
-static const struct event_log_hash_info crypto_hash_info = {
-	.func = crypto_mod_calc_hash,
-	.ids = (const uint32_t[]){ CRYPTO_MD_ID },
-	.count = 1U,
-};
+static const uint8_t *event_log_base;
 
 /* FVP table with platform specific image IDs, names and PCRs */
 const event_log_metadata_t fvp_event_log_metadata[] = {
@@ -70,21 +65,32 @@ const event_log_metadata_t fvp_event_log_metadata[] = {
 
 void bl2_plat_mboot_init(void)
 {
-	uint8_t *event_log_start;
+	uint8_t *event_log_start __unused;
 	uint8_t *event_log_finish;
 	size_t bl1_event_log_size __unused = 0;
 	size_t event_log_max_size __unused = 0;
+	struct transfer_list_entry *te __unused;
 	int rc __unused;
 
 #if TRANSFER_LIST
 	event_log_start = transfer_list_event_log_extend(
 		secure_tl, PLAT_ARM_EVENT_LOG_MAX_SIZE);
-	event_log_finish = event_log_start + PLAT_ARM_EVENT_LOG_MAX_SIZE;
 
-	event_log_base = (uintptr_t)event_log_start;
+	/*
+	 * Retrieve the extend event log entry from the transfer list, the API above
+	 * returns a cursor position rather than the base address - we need both to
+	 * init the library.
+	 */
+	te = transfer_list_find(secure_tl, TL_TAG_TPM_EVLOG);
+
+	event_log_base =
+		transfer_list_entry_data(te) + EVENT_LOG_RESERVED_BYTES;
+	event_log_finish = transfer_list_entry_data(te) + te->data_size;
+
+	bl1_event_log_size = event_log_start - event_log_base;
 #else
-	rc = arm_get_tb_fw_info(&event_log_base, &bl1_event_log_size,
-				&event_log_max_size);
+	rc = arm_get_tb_fw_info((uint64_t *)&event_log_base,
+				&bl1_event_log_size, &event_log_max_size);
 	if (rc != 0) {
 		ERROR("%s(): Unable to get Event Log info from TB_FW_CONFIG\n",
 		      __func__);
@@ -100,14 +106,12 @@ void bl2_plat_mboot_init(void)
 	 * BL1 and BL2 share the same Event Log buffer and that BL2 will
 	 * append its measurements after BL1's
 	 */
-	event_log_start =
-		(uint8_t *)((uintptr_t)event_log_base + bl1_event_log_size);
 	event_log_finish =
 		(uint8_t *)((uintptr_t)event_log_base + event_log_max_size);
 #endif
 
-	rc = event_log_init_and_reg(event_log_start, event_log_finish,
-				    &crypto_hash_info);
+	rc = event_log_init_and_reg((uint8_t *)event_log_base, event_log_finish,
+				    bl1_event_log_size, crypto_mod_tcg_hash);
 	if (rc < 0) {
 		ERROR("Failed to initialize event log (%d).\n", rc);
 		panic();
@@ -117,6 +121,9 @@ void bl2_plat_mboot_init(void)
 int plat_mboot_measure_critical_data(unsigned int critical_data_id,
 				     const void *base, size_t size)
 {
+	const event_log_metadata_t *metadata_ptr;
+	int err;
+
 	/*
 	 * It is very unlikely that the critical data size would be
 	 * bigger than 2^32 bytes
@@ -124,10 +131,16 @@ int plat_mboot_measure_critical_data(unsigned int critical_data_id,
 	assert(size < UINT32_MAX);
 	assert(base != NULL);
 
+	metadata_ptr = mboot_find_event_log_metadata(fvp_event_log_metadata,
+						     critical_data_id);
+	if (metadata_ptr == NULL) {
+		return 0;
+	}
+
 	/* Calculate image hash and record data in Event Log */
-	int err = event_log_measure_and_record((uintptr_t)base, (uint32_t)size,
-					       critical_data_id,
-					       fvp_event_log_metadata);
+	err = event_log_measure_and_record(metadata_ptr->pcr, (uintptr_t)base,
+					   size, metadata_ptr->name,
+					   strlen(metadata_ptr->name) + 1U);
 	if (err != 0) {
 		ERROR("%s%s critical data (%i)\n",
 		      "Failed to ", "record",  err);
@@ -203,7 +216,7 @@ void bl2_plat_mboot_finish(void)
 	 * Re-size the event log for the next stage and update the size to include
 	 * the entire event log (i.e., not just what this stage has added.)
 	 */
-	event_log_base = (uintptr_t)transfer_list_event_log_finish(
+	event_log_base = transfer_list_event_log_finish(
 		secure_tl, (uintptr_t)event_log_base + event_log_cur_size);
 
 	/* Ensure changes are visible to the next stage. */

plat/arm/board/fvp/fvp_common_measured_boot.c
@@ -11,6 +11,7 @@
 #include <plat/common/platform.h>
 
 #include <common/desc_image_load.h>
+#include <common/measured_boot.h>
 #include <drivers/auth/crypto_mod.h>
 #include <event_measure.h>
 #include <event_print.h>
@@ -19,13 +20,22 @@ extern event_log_metadata_t fvp_event_log_metadata[];
 
 int plat_mboot_measure_image(unsigned int image_id, image_info_t *image_data)
 {
+	const event_log_metadata_t *metadata_ptr;
 	int err;
 
+	metadata_ptr =
+		mboot_find_event_log_metadata(fvp_event_log_metadata, image_id);
+	if (metadata_ptr == NULL) {
+		ERROR("Unable to find metadata for image %u.\n", image_id);
+		return -1;
+	}
+
 	/* Calculate image hash and record data in Event Log */
-	err = event_log_measure_and_record(image_data->image_base,
+	err = event_log_measure_and_record(metadata_ptr->pcr,
+					   image_data->image_base,
 					   image_data->image_size,
-					   image_id,
-					   fvp_event_log_metadata);
+					   metadata_ptr->name,
+					   strlen(metadata_ptr->name) + 1U);
 	if (err != 0) {
 		ERROR("%s%s image id %u (%i)\n",
 		      "Failed to ", "record in event log", image_id, err);

plat/arm/board/fvp/platform.mk
@@ -117,6 +117,10 @@ $(eval $(call add_define,FVP_GICR_REGION_PROTECTION))
 # Pass FVP_TRUSTED_SRAM_SIZE to the build system.
 $(eval $(call add_define,FVP_TRUSTED_SRAM_SIZE))
 
+ifeq (${DRTM_SUPPORT},1)
+MBOOT_EL_HASH_ALG	:=	sha256
+endif
+
 # Sanity check the cluster count and if FVP_CLUSTER_COUNT <= 2,
 # choose the CCI driver , else the CCN driver
 ifeq ($(FVP_CLUSTER_COUNT), 0)
@@ -533,11 +537,13 @@ include plat/arm/common/arm_common.mk
 ifeq (${MEASURED_BOOT},1)
 BL1_SOURCES		+=	plat/arm/board/fvp/fvp_common_measured_boot.c	\
 				plat/arm/board/fvp/fvp_bl1_measured_boot.c	\
-				lib/psa/measured_boot.c
+				lib/psa/measured_boot.c	\
+				common/measured_boot_helpers.c
 
 BL2_SOURCES		+=	plat/arm/board/fvp/fvp_common_measured_boot.c	\
 				plat/arm/board/fvp/fvp_bl2_measured_boot.c	\
-				lib/psa/measured_boot.c
+				lib/psa/measured_boot.c	\
+				common/measured_boot_helpers.c
 endif
 
 ifeq (${DRTM_SUPPORT}, 1)

TrustedFirmware-A/trusted-firmware-a

commit ac18ce2bfe2c94c4265ec42586fb51a1b1fb3213

feat(fvp): fix PLAT_ARM_FW_HANDOFF_SIZE
commit 8a583b978355 (“refactor(fvp): use crypto-agile measured boot”)
changes the event log entry size calculation in BL2.
Specifically, it updates the logic from using PLAT_ARM_EVENT_LOG_MAX_SIZE
defined in BL2 to using the sum of  the BL1 event log size and
PLAT_ARM_EVENT_LOG_MAX_SIZE in BL2.

As a result of this change, allocation of the xferlist entry
for SPMC_MANIFEST fails, because PLAT_ARM_FW_HANDOFF_SIZE still
uses PLAT_ARM_EVENT_LOG_MAX_SIZE based on the BL1-defined value.

To resolve this issue, use the correct event log size
when calculating PLAT_ARM_FW_HANDOFF_SIZE.

Fixes: 8a583b978355 ("refactor(fvp): use crypto-agile measured boot")
Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>
Change-Id: Ie6e9d5a15ee4784feef566716847c9a97abc9f94

plat/arm/board/fvp/include/platform_def.h
@@ -174,11 +174,11 @@
 /*
  * PLAT_ARM_FW_HANDOFF_SIZE should be page-aligned to ensure proper xlat mapping.
  * If it is not, generating the page table mapping for FW_HANDOFF will fail.
- * Because PLAT_ARM_EVENT_LOG_MAX_SIZE is not guaranteed to be aligned,
+ * Because EVENT_LOG_ENTRY_SIZE is not guaranteed to be aligned,
  * PLAT_ARM_FW_HANDOFF_SIZE must be explicitly aligned.
  */
 #define PLAT_ARM_FW_HANDOFF_SIZE	((((PLAT_ARM_HW_CONFIG_SIZE +		\
-					    PLAT_ARM_EVENT_LOG_MAX_SIZE +	\
+					    EVENT_LOG_ENTRY_SIZE +		\
 					    PLAT_ARM_TB_FW_CONFIG_SIZE +	\
 					    PLAT_ARM_SPMC_SP_MANIFEST_SIZE) +	\
 					    PAGE_SIZE_MASK) >>			\
@@ -190,6 +190,13 @@
 
 #if RESET_TO_BL31
 #define PLAT_ARM_TRANSFER_LIST_DTB_OFFSET	FW_NS_HANDOFF_BASE + TRANSFER_LIST_DTB_OFFSET
+#define EVENT_LOG_ENTRY_SIZE			PLAT_ARM_EVENT_LOG_MAX_SIZE
+#else
+/*
+ * The maximum PLAT_ARM_EVENT_LOG_MAX_SIZE for BL2 is SZ_4K
+ * SZ_512 is maximum event log size for BL1.
+ */
+#define EVENT_LOG_ENTRY_SIZE			(SZ_512 + SZ_4K)
 #endif
 
 #else