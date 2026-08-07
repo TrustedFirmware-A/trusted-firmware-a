72d0f09eded1312bc01983f43180c2230aabb2c0
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2026-08-07 16:01:52 +0100

    fix(qti): pull in access-control backend for mem_assign
    Import the QTI access-control sources and build glue needed
    by the recent qti_syscall mem-assign changes. This adds the
    missing public header and backend objects so qti_accesscontrol_mem_t
    and qti_accesscontrol_mem_assign resolve correctly across the
    affected QTI platforms.
    
    Pulled in from mainline:
    - 6ae8fce3d (feat(kodiak): add access control driver)
    - c533f768d (feat(qti): integrate access control stub)
    - c3fff757b (refactor(qti): split accesscontrol driver into stub
                 and QTISECLIB backends)
    
    Change-Id: Ia84fd78b62ea5e7e7e7b9fa85d6baf1ab04b52a0
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- plat/qti/qcs615/platform.mk
+++ plat/qti/qcs615/platform.mk
@@ -133,6 +133,8 @@ LIB_QTI_PATH	:=	${QTI_PLAT_PATH}/qtiseclib/lib/${CHIPSET}
 # will be available in coreboot.org
 QTISECLIB_PATH ?=
 
+BL31_SOURCES	+=	drivers/qti/accesscontrol/access_control_qtiseclib.c
+
 ifeq ($(QTISECLIB_PATH),)
 # if No lib then use stub implementation for qtiseclib interface
 $(warning QTISECLIB_PATH is not provided while building, using stub implementation. \
@@ -143,4 +145,3 @@ else
 # use library provided by QTISECLIB_PATH
 LDLIBS += $(QTISECLIB_PATH)
 endif
-
